c418fb8f369c5db80ac8bcbf4408cccbb181ad5d
Tapas Mandal <alloymobileinc@gmail.com>
2023-01-29 10:49:33 -0500

    Adde a test project

---
+++ Shape/Shape/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    public class Rectangle
+    {
+        double length;
+        double width;
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double Area() { 
+            return length * width; 
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (length + width);
+        }
+    }
+}
---
+++ Shape/ShapeTest/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shape;
+namespace ShapeTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Rectangle_Area_Valid() 
+        {
+            double expected = 200;
+            Rectangle r1 = new Rectangle(20,10);
+            double actual = r1.Area();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Rectangle_Perimeter_Valid()
+        {
+            double expected = 60;
+            Rectangle r1 = new Rectangle(20, 10);
+            double actual = r1.Perimeter();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
